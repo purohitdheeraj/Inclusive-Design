440661186397113b7079008aebd2d85f3ec6372d
Dheeraj Purohit <30369664+purohitdheeraj@users.noreply.github.com>
2022-02-11 08:04:26 +0530

    Feat: Buttons, Text, List Component (#4)
    * Feat: Buttons, Text, List Component
    
    * fix: commented code removed (extra)

--- docs/index.js
+++ docs/index.js
@@ -2,7 +2,24 @@ const dropdownContent = document.getElementById('dropdown__content');
 
 const dropdownBtn = document.getElementById('dropdown__btn');
 
+const floatBtn = document.getElementById('float-btn')
 
+const scrollFunction = ()=>{
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        floatBtn.style.display = "flex";
+      } else {
+        floatBtn.style.display = "none";
+      }
+}
+function toTop() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+  }
+
+floatBtn.addEventListener('click',toTop)
+
+
+window.onscroll = () => scrollFunction();
 
 dropdownBtn.addEventListener('click',function(e){
     dropdownContent.classList.toggle("show")
